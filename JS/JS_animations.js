91676fd5722a502f17fd1952c813fd3aa4d1b2db
Mjbaggio <mmbaggs@gmail.com>
2024-10-29 23:54:18 -0500

    Add files via upload

--- JS/JS_animations.js
+++
@@ -1,9 +0,0 @@
-gsap.to('.thinking', {
-    scrollTrigger: {
-	trigger: '.Loto', // the element will trigger the scroll
-    start: "top center", //when the top center hits the viewport
-	end: "top 100px", //when the top of .Loto is 100px from the top of the viewport
-	pin: true,
-	scrub: 1
-},
-	backgroundImage: "url('fonttype2')" // changing background image
